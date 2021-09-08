3a776b3b79a9d993933e78364afc8b7fc86d8898
Artur <arturkot95@gmail.com>
2021-09-08 15:58:44 +0200

    new collection modal

---
+++ imports/ui/components/modals/NewCollectionModal.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Modal, Button, Form } from 'semantic-ui-react';
+
+export default function NewCollectionModal({ open, onClose, onOpen }) {
+  let [name, setName] = useState('');
+
+  function onCreate() {
+    if (name.trim()) {
+      Meteor.call('collection.new', name.trim());
+      
+      onClose();
+      setName('');
+    }
+  }
+
+  return <Modal
+    onClose={() => onClose()}
+    onOpen={() => onOpen()}
+    open={open}
+    size="small"
+  >
+    <Modal.Header>New Collection</Modal.Header>
+    <Modal.Content>
+      <Modal.Description>
+        <Form onSubmit={(e) => {onCreate(); e.preventDefault();}}>
+          <Form.Input value={name} onChange={(e) => setName(e.target.value)}
+               autoFocus label="Name" fluid placeholder="e.g. German..." />
+        </Form>
+      </Modal.Description>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button color='black' onClick={() => onClose()}>
+        Close
+      </Button>
+      <Button
+        content="Create"
+        labelPosition='right'
+        icon='checkmark'
+        onClick={() => onCreate()}
+        positive
+      />
+    </Modal.Actions>
+  </Modal>
+}
