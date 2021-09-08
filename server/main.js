fb808b31c87f4a9dbaff510fff5473d5953fb93f
Artur <arturkot95@gmail.com>
2021-09-08 12:40:39 +0200

    seed data

--- server/main.js
+++ server/main.js
@@ -1,4 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import '/imports/api/flashcardsMethods';
 
-Meteor.startup(() => {});
+import '/imports/api/flashcardMethods';
+import '/imports/api/collectionMethods';
+
+import Collections from '/imports/db/Collections';
+
+Meteor.startup(() => {
+  const docs = Collections.find({}).fetch();
+  if (docs.length === 0) {
+    import './seed';
+  }
+});
---
+++ server/seed.js
@@ -0,0 +1,2 @@
+Meteor.call('collection.new', 'German');
+Meteor.call('collection.new', 'English');
