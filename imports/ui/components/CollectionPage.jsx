2dd1b7a55ddc431acf77646df23dac9c9f8a4efe
Artur <arturkot95@gmail.com>
2021-09-08 21:54:09 +0200

    remove collection with confirm modal

--- imports/ui/components/CollectionPage.jsx
+++ imports/ui/components/CollectionPage.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import Collections from '/imports/db/Collections';
 import {
   Header,
-  Grid
+  Grid,
+  Label,
+  Confirm
 } from 'semantic-ui-react';
 import Flashcard from './Flashcard.jsx';
 
 export default function CollectionPage({ collectionId }) {
+  let [showConfirm, setShowConfirm] = useState(false);
   const collection = useTracker(() => Collections.find({ _id: collectionId }).fetch()[0]);
 
+  function removeCollection() {
+    Meteor.call('collection.remove', collectionId);
+    setShowConfirm(false);
+  }
+
   return <>
-    <Header size="huge">{collection.name}</Header>
+    <Confirm
+      open={showConfirm}
+      closeOnDocumentClick
+      content="Do you want to remove this collection?"
+      size="tiny"
+      confirmButton="Remove"
+      onConfirm={removeCollection}
+      onCancel={() => setShowConfirm(false)}
+    />
+
+    <Header size="huge">
+      {collection.name}
+      <Label style={{cursor: 'pointer'}} 
+        color="red" size="mini" onClick={() => setShowConfirm(true)}>
+          Remove Collection
+        </Label>
+    </Header>
     <Grid>
       { collection.flashcards.map(f => (
         <Grid.Column width={4} key={f._id}>
