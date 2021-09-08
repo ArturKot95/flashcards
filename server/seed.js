73bc45a02294e4cf94effba70d97173f879a30e1
Artur <arturkot95@gmail.com>
2021-09-08 21:15:57 +0200

    add example flashcards insert to seed file

--- server/seed.js
+++ server/seed.js
@@ -1,2 +1,10 @@
-Meteor.call('collection.new', 'German');
-Meteor.call('collection.new', 'English');
+const id1 = Meteor.call('collection.new', 'German');
+const id2 = Meteor.call('collection.new', 'English');
+
+Meteor.call('flashcard.new', id1, 'front 1', 'back 1');
+Meteor.call('flashcard.new', id1, 'front 2', 'back 2');
+Meteor.call('flashcard.new', id1, 'front 3', 'back 3');
+
+Meteor.call('flashcard.new', id2, 'front 1', 'back 1');
+Meteor.call('flashcard.new', id2, 'front 2', 'back 2');
+Meteor.call('flashcard.new', id2, 'front 3', 'back 3');
